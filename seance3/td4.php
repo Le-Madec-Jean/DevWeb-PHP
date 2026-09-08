dd7e7200256425db78e5ddd30aa290ceed0e51aa
Le Madec Jean <jean.le-madec3@etu.univ-lorraine.fr>
2026-09-08 08:21:35 +0200

    add td4 in seance3

---
+++ seance3/td4.php
@@ -0,0 +1,10 @@
+<?php
+
+$playlist = [
+    "nom"=> " my personnal best",
+    "genre"=> "rock",
+    "createur"=> " john doe",
+    "date"=> " 28-08-2022",
+    "nbpistes"=> 56,
+    "duree"=>  10080 ,
+];

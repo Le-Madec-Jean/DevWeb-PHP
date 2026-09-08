40634e8e32029a8f0b35d30da1ed2a73d72e15ae
Le Madec Jean <jean.le-madec3@etu.univ-lorraine.fr>
2026-09-08 09:16:05 +0200

    update td4

--- seance3/td4.php
+++ seance3/td4.php
@@ -1,10 +1,55 @@
 <?php
 
 $playlist = [
-    "nom"=> " my personnal best",
+    "nom"=> " my personnal best of",
     "genre"=> "rock",
     "createur"=> " john doe",
     "date"=> " 28-08-2022",
     "nbpistes"=> 56,
     "duree"=>  10080 ,
 ];
+
+
+function display(array $playlist) : void {
+    $str = "\$playlist : ";
+
+    foreach ($playlist as $key => $value) {
+        
+        
+        switch ($key) {
+            case "nom":
+                $str .= $value;
+                break;
+            case "genre":
+                $str .= "($value)";
+                break;
+            case "createur":
+                $str .= " par $value";
+                break;
+            case "date":
+                $str .= " le $value ";
+                break;
+            case "nbpistes":
+                $str .= " $value";
+                break;
+            
+            
+            case "duree":
+                $str .= " pour une durée total de $value s";
+            
+            
+            default:
+                
+                break;
+        }
+
+        
+    }
+
+
+    print $str;
+    
+}
+
+
+display($playlist);

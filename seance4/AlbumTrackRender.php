669fe61741e39f9ff8a7e41eb8f8dfaec43f19d4
Le Madec Jean <jean.le-madec3@etu.univ-lorraine.fr>
2026-09-09 09:59:38 +0200

    update

---
+++ seance4/AlbumTrackRender.php
@@ -0,0 +1,20 @@
+<?php
+require_once("AlbumTrack.php");
+
+class AlbumTrackRender 
+{
+
+    private AlbumTrack $albumTrack;
+
+    public function __construct(AlbumTrack $albumTrack_para){
+        $this->albumTrack = $albumTrack_para;
+
+    }
+
+    public function render(int $selector){
+        
+    }
+    
+}
+
+

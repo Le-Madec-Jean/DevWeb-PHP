e9b5b560bf831dc4e9ec854db641657f38be463b
Le Madec Jean <jean.le-madec3@etu.univ-lorraine.fr>
2026-09-17 18:16:53 +0200

    fin exo3

--- seance4/AlbumTrackRender.php
+++ seance4/AlbumTrackRender.php
@@ -1,20 +1,76 @@
 <?php
 require_once("AlbumTrack.php");
 
-class AlbumTrackRender 
+class AlbumTrackRender
 {
 
     private AlbumTrack $albumTrack;
 
-    public function __construct(AlbumTrack $albumTrack_para){
+    public function __construct(AlbumTrack $albumTrack_para)
+    {
         $this->albumTrack = $albumTrack_para;
 
     }
 
-    public function render(int $selector){
-        
+    public function render(int $selector): string
+    {
+        $res = "";
+        switch ($selector) {
+            case 1:
+                $res = $this->render_html(1);
+                break;
+
+            case 2:
+                $res =$this->render_html(2);
+                break;
+
+            default:
+                $res = $this->render_html(1);
+                break;
+        }
+
+        return $res;
     }
-    
+
+    private function render_html(int $selector): string
+    {
+        $res = "";
+        $data = json_decode($this->albumTrack);
+        switch ($selector) {
+            case '1':
+                $res =
+                    "<audio controls>
+                <source src= \"$data->nom_fichier\" type=\"audio/mpeg\">
+                </audio>
+                
+                <p> $data->title-$data->album </p> 
+                ";
+                break;
+
+            case "2":
+                $res =
+                    "<audio controls>
+                <source src= \"$data->nom_fichier\" type=\"audio/mpeg\">
+                </audio>
+                
+                <p>$data->numero_pistes-$data->title-$data->album </p> 
+                ";
+                break;
+
+            default:
+                $res =
+                    "<audio controls>
+                <source src= \"$data->nom_fichier\" type=\"audio/mpeg\">
+                </audio>
+                
+                <p> $data->title-$data->album </p> 
+                ";
+                break;
+        }
+
+        return $res;
+    }
+
 }
 
 
--- seance4/exo2.php
+++ seance4/exo2.php
@@ -1,11 +1,16 @@
 <?php
 
 require_once("AlbumTrack.php");
+require_once("AlbumTrackRender.php");
 
-$album = new AlbumTrack("titre", "album", "path", 1);
+$album = new AlbumTrack("titre", "audio/01-Im_with_you_BB-King-Lucille.mp3", "album", 1);
 $album2 = new AlbumTrack("titre1", "album2", "path2", 2);
 
-echo $album->title . " " . $album->album . " " . $album->nom_fichier . " " . $album->numero_pistes . "\n";
+//echo $album->title . " " . $album->album . " " . $album->nom_fichier . " " . $album->numero_pistes . "\n";
 //print_r($album->__tostring());
+//echo $album2->__tostring();
+//var_dump($album2->__tostring());
 
-var_dump($album2->__tostring());
+$trackRender = new AlbumTrackRender($album);
+
+print $trackRender->render(1);

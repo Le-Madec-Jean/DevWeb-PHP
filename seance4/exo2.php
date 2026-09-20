6ddb8f9b7a05c6c24a0e1c8bb2647dcfb64b4981
Le Madec Jean <jean.le-madec3@etu.univ-lorraine.fr>
2026-09-20 22:26:49 +0200

    fin exo4

--- seance4/exo2.php
+++ seance4/exo2.php
@@ -1,14 +1,16 @@
 <?php
+require_once("AudioTrack.php");
 
 require_once("AlbumTrack.php");
 require_once("AlbumTrackRender.php");
+require_once("PodcastTrack.php");
 
 $album = new AlbumTrack("titre", "audio/01-Im_with_you_BB-King-Lucille.mp3", "album", 1);
-$album2 = new AlbumTrack("titre1", "album2", "path2", 2);
+$podcast = new PodcastTrack("20/09/2026", "titre", "path");
 
 //echo $album->title . " " . $album->album . " " . $album->nom_fichier . " " . $album->numero_pistes . "\n";
 //print_r($album->__tostring());
-//echo $album2->__tostring();
+echo $podcast->__tostring();
 //var_dump($album2->__tostring());
 
 $trackRender = new AlbumTrackRender($album);
